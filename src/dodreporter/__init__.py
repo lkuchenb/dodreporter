900567d8de1b6314cdcf0725b66fa9223beb1469
Leon Kuchenbecker <lk@lkit.eu>
2020-10-23 20:58:20 +0200

    Add SMTP client to runner, minor changes

--- src/dodreporter/__init__.py
+++ src/dodreporter/__init__.py
@@ -27,63 +27,80 @@ import os
 ####################################################################################################
 
 from dodreporter.error import DODReporterConfigError, DODReporterError
-from dodreporter import config, log
+from dodreporter import config, log, SMTPClient
 
 ####################################################################################################
 
 class DODCryptRunner(threading.Thread):
 
-    def __init__(self, settings : config.DODSettings, terminate_event : threading.Event):
+    def notify_crypt_unavailable(self, paths):
+        pass
+
+    def notify_crypt_available(self):
+        pass
+
+    def __init__(self, settings : config.DODSettings, reporter):
         threading.Thread.__init__(self)
-        self.terminate_event = terminate_event
-        self.enabled = True
-        self.paths = settings.global_settings.crypt_dirs
-        self.failmail = False
-        self.last_status = None
+        self.enabled           = True
+        self.paths             = settings.global_settings.crypt_dirs
+        self.failmail          = False
+        self.last_status       = None
+        self.reporter          = reporter
 
     def check(self):
         status = [ os.path.exists(p) for p in self.paths ]
-        for path, exists in zip(self.paths, status):
-            if status != self.last_status:
+
+        # Log current state if it has changed from the previous observed state
+        if status != self.last_status:
+            for path, exists in zip(self.paths, status):
                 if exists:
                     log.log(f"[crypt_run] Crypt path '{path}' exists.")
                 else:
                     log.log(f"[crypt_run] Crypt path '{path}' does not exist.")
+
+        # Keep in mind last state for future checks
         self.last_status = status
-        return all(status)
+
+        # Return all missing paths
+        return [ path for path, exists in zip(self.paths, status) if not exists ]
 
     def run(self):
         if not self.paths:
             log.log("[crypt_run] Crypt runner not starting up, no crypt dir paths configured.")
             return
         log.log("[crypt_run] Crypt runner started.")
-        while not self.terminate_event.is_set():
-            if self.check():
+        while not self.reporter.terminate_event.is_set():
+            failed_paths = self.check()
+            if not failed_paths:
                 log.log("[crypt_run] All crypt paths are available. Crypt runner terminating.")
                 return
             else:
                 if not self.failmail:
-                    log.log("[crypt_run] Missing crypt paths.")
+                    log.log("[crypt_run] Missing crypt paths. Sending out email notification.")
+                    self.notify_crypt_unavailable(failed_paths)
                     self.failmail = True
-            self.terminate_event.wait(3)
+            self.reporter.terminate_event.wait(3)
 
 ####################################################################################################
 
 class DODHostRunner(threading.Thread):
 
-    def __init__(self, settings : config.DODSettings, terminate_event : threading.Event):
+    def __init__(self, settings : config.DODSettings, reporter):
         threading.Thread.__init__(self)
-        self.terminate_event = terminate_event
+        self.reporter = reporter
 
     def run(self):
         log.log("[host_run] Host runner started.")
-        while not self.terminate_event.is_set():
-            self.terminate_event.wait(3)
+        while not self.reporter.terminate_event.is_set():
+            self.reporter.terminate_event.wait(3)
         pass
 
 ####################################################################################################
 
 class DODReporter:
+
+    terminate_event = threading.Event()
+    smtp_lock       = threading.Lock()
 
     def terminate(self):
         self.terminate_event.set()
@@ -94,12 +111,28 @@ class DODReporter:
         for t in self.threads:
             t.join()
 
+    def smtp_send(*args):
+        with smtp_lock:
+            self.smtp_client.send_message(*args)
+
     def __init__(self, config):
-        self.terminate_event = threading.Event()
+        gs = config.global_settings
+
+        # Set up smtp client
+        self.smtp_client = SMTPClient.SMTPClient(
+                hostname = gs.smtp_host,
+                port     = gs.smtp_port,
+                user     = gs.smtp_user,
+                password = gs.smtp_pass,
+                tls      = not gs.smtp_no_tls)
+
+        # Set up runner threads
         self.threads = [
-                DODCryptRunner(config, self.terminate_event),
-                DODHostRunner(config, self.terminate_event)
+                DODCryptRunner(config, self),
+                DODHostRunner(config, self)
                 ]
+
+        # Start runner threads
         for t in self.threads:
             t.start()
 
